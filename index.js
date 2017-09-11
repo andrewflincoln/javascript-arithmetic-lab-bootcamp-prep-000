04fa2578819ae0802e11ba479d3cf94232954a62
Andrew Lincoln <andrewjlincoln@gmail.com>
2017-09-11 19:50:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 
 function add(a, b) {
-  return parseInt(a) + parseInt(b)
+  return a + b
 }
 
 function subtract (a, b) {
   return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
 }
